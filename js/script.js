eeb717bb76687e9e7117a33fee1d616e77a5a4c1
oreakintobi <oreakintobi@gmail.com>
2019-09-19 11:58:46 +0100

    deleted inactive and unecessary code

--- js/script.js
+++ js/script.js
@@ -36,34 +36,3 @@ function updateStatus(id, status) {
             console.log(error);
         })
 }
-
-
-// leaveRequests.addRequest should add objects
-var leaveRequests = {
-    requests: [],
-    displayRequests: function() {
-        console.log("Current Leave Requests:", this.requests);
-    },
-    addRequest: function(request) {
-        this.requests.push({
-            request: request,
-            approved: false
-        });
-        this.displayRequests();
-    },
-    // leaveRequests.changeRequest should change the request property
-    changeRequest: function(position, request) {
-        this.requests[position].request = request;
-        this.displayRequests();
-    },
-    deleteRequest: function(position) {
-        this.requests.splice(position, 1);
-        this.displayRequests();
-    },
-    // leaveRequests.toggleApproval should change the approved property
-    toggleApproval: function(position) {
-        var request = this.requests[position];
-        request.approved = !request.approved;
-        this.displayRequests();
-    }
-}
